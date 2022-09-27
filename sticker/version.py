06cfb58ccea65ce211599ea35d4e2c226a19a7ad
Gabriel Marincu <contact@gabimarincu.xyz>
2022-09-27 21:36:15 +0300

    Test stickerpack2

--- sticker/version.py
+++ sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = '99ced887'
-version = '0.1.0+dev.99ced887'
-linkified_version = '0.1.0+dev.[99ced887](https://github.com/maunium/stickerpicker/commit/99ced8878aaf89a47dd8bdb592025ae2160d6f07)'
+git_revision = '46bd670c'
+version = '0.1.0+dev.46bd670c'
+linkified_version = '0.1.0+dev.[46bd670c](https://github.com/maunium/stickerpicker/commit/46bd670cf98b7a9cfcc1e8ec763214336dc557c9)'
